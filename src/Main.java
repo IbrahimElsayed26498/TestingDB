145fc53582667a2fad0c28b978761f594507e19e
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-06 14:48:00 +0200

    Adding 'closing connection'statments.

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,45 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection connection;
-        Statement statement;
+        Connection connection = null;
+        Statement statement = null;
         ResultSet resultSet = null;
 
-        // connecting to the database
-        connection = DBInfo.connect();
+        try{
+            // connecting to the database
+            connection = DBInfo.connect();
 
-        // write the query
-        statement = connection.createStatement(
-                resultSet.TYPE_SCROLL_INSENSITIVE,
-                resultSet.CONCUR_READ_ONLY);
+            // write the query
+            statement = connection.createStatement(
+                    resultSet.TYPE_SCROLL_INSENSITIVE,
+                    resultSet.CONCUR_READ_ONLY);
 
-        String query = "SELECT * from city";
+            String query = "SELECT * from city";
 
-        // run the query
-        resultSet = statement.executeQuery(query);
+            // run the query
+            resultSet = statement.executeQuery(query);
 
-        //Moves the cursor to the last row in this ResultSet object.
-        resultSet.last();
+            //Moves the cursor to the last row in this ResultSet object.
+            resultSet.last();
 
+            // print number of rows.
+            System.out.println(resultSet.getRow());
 
-        System.out.println(resultSet.getRow());
+        }catch (Exception e){
+
+            System.err.println(e);
+
+        }finally {
+
+            if(connection != null)
+                connection.close();
+
+            if(statement != null)
+                statement.close();
+
+            if(resultSet != null)
+                resultSet.close();
+        }
 
 
     }
