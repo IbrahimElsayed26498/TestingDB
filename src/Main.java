af59523ddf762c7daf1ae46c5b330035e04cb26d
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-06 16:27:22 +0200

    Printing all columns ia specific row.

--- src/Main.java
+++ src/Main.java
@@ -15,21 +15,35 @@ public class Main {
             statement = connection.createStatement(
                     resultSet.TYPE_SCROLL_INSENSITIVE,
                     resultSet.CONCUR_READ_ONLY);
-
+            System.out.println("Connected!");
             String query = "SELECT * from city";
 
             // run the query
             resultSet = statement.executeQuery(query);
 
             //Moves the cursor to the last row in this ResultSet object.
-            resultSet.last();
+            //resultSet.last();
 
             // print number of rows.
             System.out.println(resultSet.getRow());
 
+            System.out.printf("%-5s %-15s %-3s %-15s %-7s%n",
+                    "ID", "Name", "CountryCode",
+                    "District", "Population");
+
+            // select all the columns.
+            while(resultSet.next()){
+                System.out.printf("%-5d %-33s %-3s %-20s %-7d%n",
+                        resultSet.getInt("ID"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("CountryCode"),
+                        resultSet.getString("District"),
+                        resultSet.getInt("Population"));
+            }
+
         }catch (Exception e){
 
-            System.err.println(e);
+            System.out.println(e.getMessage());
 
         }finally {
 
