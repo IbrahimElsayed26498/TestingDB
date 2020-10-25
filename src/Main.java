38ed1fea98c5823c1d86566d737ae98b0e006116
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-25 15:01:08 +0200

    select specific number of rows.

--- src/Main.java
+++ src/Main.java
@@ -13,33 +13,29 @@ public class Main {
 
             // write the query
             statement = connection.createStatement(
-                    resultSet.TYPE_SCROLL_INSENSITIVE,
+                    resultSet.TYPE_SCROLL_INSENSITIVE, // make the resultSet cursor scrollable.
                     resultSet.CONCUR_READ_ONLY);
+            statement.setMaxRows(30);
             System.out.println("Connected!");
-            String query = "SELECT * from city";
+            String query = "SELECT * from city " + "limit 4,4";
 
             // run the query
             resultSet = statement.executeQuery(query);
 
-            //Moves the cursor to the last row in this ResultSet object.
-            //resultSet.last();
 
             // print number of rows.
+            System.out.print("Number of rows: ");
+            resultSet.last(); //Moves the cursor to the last row in this ResultSet object.
             System.out.println(resultSet.getRow());
 
-            System.out.printf("%-5s %-15s %-3s %-15s %-7s%n",
+
+            System.out.printf("%-5s %-33s %-12s %-20s %-7s%n",
                     "ID", "Name", "CountryCode",
                     "District", "Population");
 
             // select all the columns.
-            while(resultSet.next()){
-                System.out.printf("%-5d %-33s %-3s %-20s %-7d%n",
-                        resultSet.getInt("ID"),
-                        resultSet.getString("Name"),
-                        resultSet.getString("CountryCode"),
-                        resultSet.getString("District"),
-                        resultSet.getInt("Population"));
-            }
+            printTable(resultSet);
+
 
         }catch (Exception e){
 
@@ -58,5 +54,17 @@ public class Main {
         }
 
 
+    }
+
+    private static void printTable(ResultSet resultSet) throws SQLException {
+        resultSet.beforeFirst();
+        while(resultSet.next()){
+            System.out.printf("%-5d %-33s %-12s %-20s %-7d%n",
+                    resultSet.getInt("ID"),
+                    resultSet.getString("Name"),
+                    resultSet.getString("CountryCode"),
+                    resultSet.getString("District"),
+                    resultSet.getInt("Population"));
+        }
     }
 }
